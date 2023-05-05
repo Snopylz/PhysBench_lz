c372335a403e14e3d3faba79c977af12bf9ee934
KegangWangCCNU <132054009+KegangWangCCNU@users.noreply.github.com>
2023-05-05 17:06:13 +0800

    Add files via upload

--- dataset_process/generate_cache.py
+++ dataset_process/generate_cache.py
@@ -4,7 +4,7 @@ from utils import *
 from multiprocessing import Pool
 import tqdm
 
-cores = 8
+cores = 4
 
 df = pd.read_csv('../datasets/CCNU_dataset_index.csv')
 files_ccnu = df[(df['codec']=='MJPG')&(df['fold']>=0)]['file']
@@ -15,12 +15,21 @@ files_pure = df['file']
 df = pd.read_csv('../datasets/UBFC_rPPG2_dataset_index.csv')
 files_ubfc_rppg2 = df['file']
 
-def cache(f, vid, overwrite=False):
+df = pd.read_csv('../datasets/SCAMPS_dataset_index.csv')
+files_scamps = df['file']
+
+def log(s):
+    with open('cache_log.txt', 'a') as f:
+        f.write(s+'\n')
+
+def cache(f, vid, overwrite=True):
+    path=f
     if overwrite or not os.path.exists(f'{tmp}/{hash(f)}.h5'):
         with h5py.File(f'{tmp}/{hash(f)}.h5', 'w') as f:
-            boxes, landmarks = generate_vid_labels(vid)
+            boxes, landmarks = generate_vid_labels(vid, detect_per_n=5)
             f.create_dataset('boxes', data=np.array(boxes), compression=0)
             f.create_dataset('landmarks', data=np.array(landmarks), compression=0)
+            log(f'{path} cached.')
 
 def cache_ccnu(f):
     vid, bvp, ts = loader_ccnu(f)
@@ -34,8 +43,13 @@ def cache_ubfc_rppg2(f):
     vid, bvp, ts = loader_ubfc_rppg2(f)
     cache(loader_ubfc_rppg2.base+f, vid)
 
+def cache_scamps(f):
+    vid, bvp, ts = loader_scamps(f)
+    cache(loader_scamps.base+f, vid)
+
 if __name__ == '__main__':
     with Pool(cores) as p:
         #p.map(cache_ccnu, files_ccnu)
         #p.map(cache_pure, files_pure)
-        p.map(cache_ubfc_rppg2, files_ubfc_rppg2)
+        #p.map(cache_ubfc_rppg2, files_ubfc_rppg2)
+        p.map(cache_scamps, files_scamps)
