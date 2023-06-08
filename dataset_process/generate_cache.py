ab9c50e9674ba323cfa321e9db1b3923932b3cbb
汪汪 <132054009+KegangWangCCNU@users.noreply.github.com>
2023-06-08 22:22:57 +0800

    Add files via upload

--- dataset_process/generate_cache.py
+++ dataset_process/generate_cache.py
@@ -4,7 +4,7 @@ from utils import *
 from multiprocessing import Pool
 import tqdm
 
-cores = 4
+cores = 8
 
 df = pd.read_csv('../datasets/CCNU_dataset_index.csv')
 files_ccnu = df[(df['codec']=='MJPG')&(df['fold']>=0)]['file']
@@ -21,6 +21,9 @@ files_scamps = df['file']
 df = pd.read_csv('../datasets/UBFC_PHYS_dataset_index.csv')
 files_ubfc_phys = df['file']
 
+df = pd.read_csv('../datasets/COHFACE_dataset_index.csv')
+files_cohface = df['file']
+
 def log(s):
     with open('cache_log.txt', 'a') as f:
         f.write(s+'\n')
@@ -32,7 +35,7 @@ def cache(f, vid, overwrite=True):
             boxes, landmarks = generate_vid_labels(vid, detect_per_n=5)
             f.create_dataset('boxes', data=np.array(boxes), compression=0)
             f.create_dataset('landmarks', data=np.array(landmarks), compression=0)
-            log(f'{path} cached.')
+            log(f'{path} cached. {len(boxes)} frames')
 
 def cache_ccnu(f):
     vid, bvp, ts = loader_ccnu(f)
@@ -54,10 +57,15 @@ def cache_ubfc_phys(f):
     vid, bvp, ts = loader_ubfc_phys(f)
     cache(loader_ubfc_phys.base+f, vid)
 
+def cache_cohface(f):
+    vid, bvp, ts, ext = loader_cohface(f)
+    cache(loader_cohface.base+f, vid)
+
 if __name__ == '__main__':
     with Pool(cores) as p:
         #p.map(cache_ccnu, files_ccnu)
         #p.map(cache_pure, files_pure)
         #p.map(cache_ubfc_rppg2, files_ubfc_rppg2)
         #p.map(cache_scamps, files_scamps)
-        p.map(cache_ubfc_phys, files_ubfc_phys)
+        #p.map(cache_ubfc_phys, files_ubfc_phys)
+        p.map(cache_cohface, files_cohface)
